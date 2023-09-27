edb843440dce17a23dfa875e04243af9e2c63bbc
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-27 16:07:56 +0200

    wip

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv").config();
 
-// ℹ️ Connects to the database
+require("dotenv").config();
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
+const { isAuthenticated } = require("./middleware/jwt.middleware");
+
 const express = require("express");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-// 👇 Start handling routes here
+
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+
+
+const usersRouter = require("./routes/users.routes");
+app.use("/api", isAuthenticated, usersRouter); 
+
+
+
+const authRouter = require("./routes/auth.routes");     
+app.use("/auth", authRouter); 
+
 require("./error-handling")(app);
 
 module.exports = app;
