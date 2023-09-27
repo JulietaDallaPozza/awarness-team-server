88536c99017ed5da0c0c518b42246f7a3c569ab4
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-27 16:55:15 +0200

    wip

--- models/User.model.js
+++ models/User.model.js
@@ -9,6 +9,10 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
+    name: {
+      type: String,
+      required: true
+    },
     password: {
       type: String,
       required: [true, 'Password is required.']
